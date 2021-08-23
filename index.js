ee1c07115a49f2f2a930e84823aa7450ece3c66b
Edgaras98 <edgarasstepanovas14@gmail.com>
2021-08-23 14:23:52 +0300

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 
 const express = require("express");
 
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 
 require("dotenv").config();
 
@@ -10,9 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const URI =
-  "mongodb+srv://edgariux998:edgaras123@cluster0.oekth.mongodb.net/demo5";
-
+const URI = process.env.URI;
 const client = new MongoClient(URI);
 
 //gauname visus produktus
@@ -30,21 +28,51 @@ app.get("/", async (req, res) => {
 
 //Vieno producto gavimas
 
-app.get("/products/:_id", async (req, res) => {
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
   try {
     const con = await client.connect();
-    const data = await con
+    const dbRes = await con
       .db("demo5")
       .collection("products")
-      .find({ id: req.params.id })
+      .find(ObjectId(id))
       .toArray();
     await con.close();
 
-    return res.send(data);
+    return res.send(dbRes);
+  } catch (err) {
+    return res.status(500).send({ err });
+  }
+});
+//gauname orderius
+app.get("/orders", async (req, res) => {
+  try {
+    const con = await client.connect();
+    const dbRes = await con.db("demo6").collection("orders").find().toArray();
+    await con.close();
+    return res.send(dbRes);
   } catch (err) {
     return res.status(500).send({ err });
   }
 });
 
-const port = 8080;
+//Postinam nauja
+
+app.post("/orders", async (req, res) => {
+  try {
+    const con = await client.connect();
+    const response = await con.db("demo6").collection("orders").insertOne({
+      name: req.body.name,
+      email: req.body.email,
+      product_id: req.body.product_id,
+      price: req.body.price,
+    });
+    await con.close();
+    res.send(response);
+  } catch (e) {
+    res.status(400).send({ msg: "failed" });
+  }
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Server is running on port " + port));
